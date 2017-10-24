4e8188e54698aeb599d114eb6829ea9195575e57
flower <flower@flowerui-MacBook-Pro.local>
2017-10-24 17:48:34 +0900

    for Collector

---
+++ src/kdy/FilterClass.py
@@ -0,0 +1,48 @@
+#-*- coding : utf-8 -*-
+
+"""
+    Author : Flower
+    Time attack
+        2017.10.24 ~
+    Start log
+        2017.10.24  [start]
+"""
+
+class FilterClass():
+    def __init__(self):
+        pass
+
+    def get_re(self):
+        pass
+
+    def set_re(self):
+        pass
+
+    def get_cur(self):
+        pass
+
+    def close_cur(self):
+        pass
+
+    def select_sender(self):
+        pass
+
+    def create_table(self):
+        pass
+
+    def insert_log(self):
+        pass
+
+    def get_conn(self):
+        pass
+
+    def close_conn(self):
+        pass
+
+    def run(self):
+        pass
+
+
+if __name__ == '__main__':
+    FC = FilterClass()
+    FC.run()
