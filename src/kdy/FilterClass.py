d749675c01f4bb6851f444d1390f7931a33f9d05
flower <flower@flowerui-MacBook-Pro.local>
2017-10-29 21:22:23 +0900

    [M] FilterClass

--- src/kdy/FilterClass.py
+++ src/kdy/FilterClass.py
@@ -8,21 +8,52 @@
         2017.10.24  [start]
 """
 
+import re
+import psycopg2
+import configparser
+
+
 class FilterClass():
     def __init__(self):
         pass
 
+    def get_config(self, keyword):
+            cf = configparser.ConfigParser()
+            cf.config('./config.ini')
+        if keyword.lower() is 'default':
+            pass
+        elif keyword.lower() is 'db':
+            db_name = cf['db']['db_name']
+            user    = cf['db']['user']
+            host    = cf['db']['host']
+            passwd  = cf['db']['passwd']
+            return db_name, user, host, passwd
+        elif keyword.lower() is 'redis':
+            host = cf['redis']['host']
+            port = cf['redis']['port']
+            db   = cf['redis']['db']
+            return host, int(port), int(db)
+        elif keyword.lower() is 're':
+            return cf['re']['chk_str']
+
     def get_re(self):
-        pass
+        return re.compile(r'%s' % self.get_config('re'))
 
     def set_re(self):
         pass
 
-    def get_cur(self):
-        pass
+    def get_redis(self):
+        return redis.Redis(host='%s', port=%d, db=%d % self.get_config('redis'))
 
-    def close_cur(self):
-        pass
+    def get_cur(self):
+        conn_str = "db_name='%s' user='%s' host='%s' password='%s'"
+        conn_str = conn_str % self.get_config('db')
+        self.conn = psycopg2.connect(conn_str)
+        return cursor = self.conn.cursor()
+
+    def close_cur(self, cursor):
+        cursor.close()
+        self.conn_close()
 
     def select_sender(self):
         pass
@@ -31,12 +62,6 @@ class FilterClass():
         pass
 
     def insert_log(self):
-        pass
-
-    def get_conn(self):
-        pass
-
-    def close_conn(self):
         pass
 
     def run(self):
