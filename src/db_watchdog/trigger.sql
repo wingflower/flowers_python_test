e6eed65fb49bc7be347da8cd98959da574e41b90
flower <flower@flowerui-MacBook-Pro.local>
2017-11-13 22:50:50 +0900

    [D][M] db_watchdog mgmt

--- src/db_watchdog/trigger.sql
+++ src/db_watchdog/trigger.sql
@@ -1,18 +1,26 @@
-﻿DROP EVENT TRIGGER notify_create_table;
-DROP FUNCTION notify_create_table();
+﻿DROP EVENT TRIGGER etg_notify_create_table;
+DROP FUNCTION fn_notify_create_table();
 
-CREATE OR REPLACE FUNCTION notify_create_table()
+-----
+
+CREATE OR REPLACE FUNCTION fn_notify_create_table()
   RETURNS event_trigger AS
 $func$
-BEGIN
 
-PERFORM pg_notify('mymessage', 'new table');
+DECLARE r RECORD;
+
+BEGIN
+FOR r IN SELECT * FROM pg_event_trigger_ddl_commands() LOOP
+        RAISE NOTICE 'caught % event on %', r.command_tag, r.object_identity;
+        PERFORM pg_notify('mymessage', r.object_identity);
+    END LOOP;
 
 END
 $func$ LANGUAGE plpgsql;
+-----
 
 -----  
-CREATE EVENT TRIGGER notify_create_table ON ddl_command_end
+CREATE EVENT TRIGGER etg_notify_create_table ON ddl_command_end
    WHEN tag IN ('CREATE TABLE')
-   EXECUTE PROCEDURE notify_create_table();
+   EXECUTE PROCEDURE fn_notify_create_table();
 -----
--- src/db_watchdog/trigger_test.py
+++ src/db_watchdog/trigger_test.py
@@ -6,40 +6,15 @@ from time import sleep
 import pgpubsub
 
 
-pubsub = pgpubsub.connect(user='postgres', database='testdb', host='localhost', port=5432)
+pubsub = pgpubsub.connect(user='postgres', database='testdb', host='10.101.30.117', port=5432,
+        password='1111')
 
 while True:
     pubsub.listen('mymessage')
     temp = pubsub.get_event()
     if temp:
-        print(type(temp))
-        print(temp)
+        temp = str(temp)
+        print(temp.split(',')[-1].strip().split("'")[1])
     sleep(3)
 
 
-"""
-conn_string = "host='127.0.0.1' dbname='testdb' user='postgres' password='1111' port='5432' "
-# use our connection values to establish a connection
-conn = psycopg2.connect(conn_string)
-# create a psycopg2 cursor that can execute queries
-cursor = conn.cursor()
-# create a new table with a single column called "name"
-cursor.execute('listen mymessage')
-# run a SELECT statement - no data in there, but we can try it
-try:
-    print("Waiting for notifications on channel 'test'")
-    while True:
-	if select.select([conn],[],[],5) == ([],[],[]):
-	    print("Timeout")
-	else:
-	    conn.poll()
-	    while conn.notifies:
-		notify = conn.notifies.pop(0)
-		print("Got NOTIFY:", notify.pid, notify.channel, notify.payload)
-    #while True:
-    #    print(cursor)
-    #    rows = cursor.fetchall()
-    #    print(rows)
-except Exception as e:
-    print(e)
-"""
