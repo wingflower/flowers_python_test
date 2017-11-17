f291aa610b7be91f1d7349eb0dedb4e940fe42b0
flower <flower@flowerui-MacBook-Pro.local>
2017-11-17 17:08:14 +0900

    [M] db_watchdog, mgmt

---
+++ src/db_watchdog/DBWatchdog.py
@@ -0,0 +1,28 @@
+#-*- coding : utf-8 -*-
+
+"""
+    Author : Flower
+    Time attack
+        2017.10.10 ~
+    Start log
+        2017.10.10  [start]
+"""
+
+import os
+import sys
+
+
+class DBWatchdog():
+    def __init__(self):
+        pass
+
+    def main(self):
+        pass
+
+    def run(self):
+        pass
+
+
+if __name__ == '__main__':
+    DW = DBWatchdog()
+    DW.run()
---
+++ src/mgmt/MgmtServer.py
@@ -0,0 +1,27 @@
+#-*- coding : utf-8 -*-
+"""
+    Author : Flower
+    Time attack
+        2017.10.10 ~
+    Start log
+        2017.10.10  [start]
+"""
+
+import os
+import sys
+
+
+class MgmtServer():
+    def __init__(self):
+        pass
+
+    def main(self):
+        pass
+
+    def run(self):
+        pass
+
+
+if __name__ == '__main__':
+    MS = MgmtServer()
+    MS.run()
